55afcd79c337a509541b168a70fcec6f732b21b7
Mikael Simonsson <minemail7@gmail.com>
2020-03-06 04:48:17 +0000

    L:106 completed, cart component

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -4,6 +4,7 @@ import {Link} from 'react-router-dom';
 
 
 import { auth } from '../../firebase/firebase.utils';
+import CartIcon from '../cart-icon/cart-icon.component';
 
 import { ReactComponent as Logo } from '../../assets/crown.svg'
 
@@ -26,11 +27,13 @@ const Header = ( {currentUser }) => (
             {
                 currentUser ?
                 <div className='option' onClick={() => auth.signOut()}>SIGN OUT</div>
-                :
+                : (
                 <Link className='option' to='/signin'>
                 SIGN IN
                 </Link>
-            }
+                )}
+                <CartIcon/>
+
         </div>
     </div>
 );
