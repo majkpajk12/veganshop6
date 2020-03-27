e86b8e288e185e65150c7b9fdd6086682fb033cb
Mikael Simonsson <minemail7@gmail.com>
2020-03-27 00:35:59 +0000

    Lesson 163, code to adding shop data to Redux

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -54,6 +54,26 @@ const config = {
         return await batch.commit();
       };
 
+      export const convertCollectionsSnapshotToMaps = collectionsSnapshot => {
+        const transformedCollection = collectionsSnapshot.docs.map(docSnapshot => {
+          const { title, items } = docSnapshot.data();
+        
+        return {
+          routeName: encodeURI(title.toLowerCase()),
+          id: docSnapshot.id,
+          title,
+          items
+        };
+        });
+      
+
+      return transformedCollection.reduce((accumulator, collection) => {
+        accumulator[collection.title.toLowerCase()] = collection;
+        return accumulator;
+      } , {});
+      };
+
+
     
       export const auth = firebase.auth();
       export const firestore = firebase.firestore();
