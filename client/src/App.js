9d6712ffea4d3770be540eb46dfaed8643f50f00
Mikael Simonsson <minemail7@gmail.com>
2020-04-17 01:47:44 +0100

    L:243 Mobile Friendly App

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,18 @@
+
 import React, { useEffect } from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
-import './App.css';
-
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import CheckoutPage from './pages/checkout/checkout.component';
-
 import Header from './components/header/header.component';
 
-import { selectCurrentUser } from './redux/user/user.reselectors';
+import { GlobalStyle } from './global.styles';
+
+import { selectCurrentUser } from './redux/user/user.selectors';
 import { checkUserSession } from './redux/user/user.actions';
 
 const App = ({ checkUserSession, currentUser }) => {
@@ -21,9 +21,10 @@ const App = ({ checkUserSession, currentUser }) => {
   }, [checkUserSession]);
 
   return (
-      <div>
-        <Header />
-        <Switch>
+    <div>
+      <GlobalStyle />
+      <Header />
+      <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
           <Route exact path='/checkout' component={CheckoutPage} />
